afa3718532de0e29b333a43642c152a3bd46e62c
Gabriel L Z <gabriellabritz@gmail.com>
2026-03-24 11:30:26 -0300

    test: add unit tests for TutorService updateTutorById method

--- src/test/java/br/com/gabriel_labritz/adopet/services/TutorServiceTest.java
+++ src/test/java/br/com/gabriel_labritz/adopet/services/TutorServiceTest.java
@@ -2,11 +2,15 @@ package br.com.gabriel_labritz.adopet.services;
 
 import br.com.gabriel_labritz.adopet.dto.tutor.TutorRequestDto;
 import br.com.gabriel_labritz.adopet.dto.tutor.TutorResponseDto;
+import br.com.gabriel_labritz.adopet.dto.tutor.TutorUpdateDto;
 import br.com.gabriel_labritz.adopet.exceptions.DuplicationExistsException;
 import br.com.gabriel_labritz.adopet.exceptions.NotFoundException;
 import br.com.gabriel_labritz.adopet.infrastructure.entities.Tutor;
 import br.com.gabriel_labritz.adopet.infrastructure.repositories.TutorRepository;
-import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.ArgumentCaptor;
 import org.mockito.Captor;
@@ -31,9 +35,6 @@ class TutorServiceTest {
 
     @Captor
     private ArgumentCaptor<Tutor> tutorCaptor;
-
-    @Captor
-    private ArgumentCaptor<Long> tutorIdCaptor;
 
     private TutorRequestDto tutorRequestDto;
 
@@ -78,7 +79,7 @@ class TutorServiceTest {
         void shouldCallTutorRepositorySaveWithCorrectValues() {
             // Arrange
             when(tutorRepository.existsByEmail(tutorRequestDto.email())).thenReturn(false);
-            when(tutorRepository.save(any())).thenReturn(tutor);
+            when(tutorRepository.save(any())).thenAnswer(i -> i.getArgument(0));
 
             // Act
             tutorService.register(tutorRequestDto);
@@ -133,6 +134,106 @@ class TutorServiceTest {
             // Assert
             verify(tutorRepository).findById(tutorId);
             assertEquals(tutor, result);
+        }
+    }
+
+    @Nested
+    class getTutorById {
+        @Test
+        @DisplayName("Deve retornar com sucesso um tutor pelo id")
+        void shouldReturnATutorById() {
+            // Arrange
+            Long tutorId = 2L;
+            when(tutorRepository.findById(tutorId)).thenReturn(Optional.of(tutor));
+
+            // Act
+            TutorResponseDto result = tutorService.getTutorById(tutorId);
+
+            // Assert
+            verify(tutorRepository).findById(tutorId);
+            assertNotNull(result);
+            assertEquals(tutorResponseDto, result);
+        }
+    }
+
+    @Nested
+    class updateTutorById {
+        @Test
+        @DisplayName("Não deve chamar existsByEmail quando o email não é enviado.")
+        void shouldNotCallExistsByEmailWhenEmailIsNotSend() {
+            // Arrange
+            Long tutorId = 2L;
+            TutorUpdateDto dtoUpdate = new TutorUpdateDto(null, "11988888888");
+
+            when(tutorRepository.findById(tutorId)).thenReturn(Optional.of(tutor));
+            when(tutorRepository.save(any())).thenAnswer(i -> i.getArgument(0));
+
+            // Act
+            tutorService.updateTutorById(tutorId, dtoUpdate);
+
+            // Assert
+            verify(tutorRepository, never()).existsByEmail(any());
+            verify(tutorRepository).save(any());
+        }
+
+        @Test
+        @DisplayName("Não deve chamar existsByEmail quando o email que será atualizado é o mesmo que já está sendo usado pelo tutor.")
+        void shouldNotCallExistsByEmailWhenEmailIsEquals() {
+            // Arrange
+            Long tutorId = 2L;
+            TutorUpdateDto dtoUpdate = new TutorUpdateDto(tutor.getEmail(), "11988888888");
+
+            when(tutorRepository.findById(tutorId)).thenReturn(Optional.of(tutor));
+            when(tutorRepository.save(any())).thenAnswer(i -> i.getArgument(0));
+
+            // Act
+            tutorService.updateTutorById(tutorId, dtoUpdate);
+
+            // Assert
+            verify(tutorRepository, never()).existsByEmail(any());
+            verify(tutorRepository).save(any());
+        }
+
+        @Test
+        @DisplayName("Deve lançar uma DuplicationExistsException quando o email informado para a atualização já está em uso.")
+        void shouldThrowDuplicationExistsExceptionWhenEmailAlreadyUsed() {
+            // Arrange
+            Long tutorId = 2L;
+            TutorUpdateDto dtoUpdate = new TutorUpdateDto("emailupdate@test.com", "11988888888");
+
+            when(tutorRepository.findById(tutorId)).thenReturn(Optional.of(tutor));
+            when(tutorRepository.existsByEmail(dtoUpdate.email())).thenReturn(true);
+
+            // Act + Assert
+            assertThrows(DuplicationExistsException.class, () -> tutorService.updateTutorById(tutorId, dtoUpdate));
+            verify(tutorRepository, never()).save(any());
+        }
+
+        @Test
+        @DisplayName("Deve atualizar os dados do tutor com sucesso.")
+        void shouldUpdateTutorDataWithSuccess() {
+            // Arrange
+            Long tutorId = 2L;
+            TutorUpdateDto dtoUpdate = new TutorUpdateDto("emailupdate@test.com", "11988888888");
+
+            when(tutorRepository.findById(tutorId)).thenReturn(Optional.of(tutor));
+            when(tutorRepository.existsByEmail(dtoUpdate.email())).thenReturn(false);
+            when(tutorRepository.save(any())).thenAnswer(i -> i.getArgument(0));
+
+            // Act
+            TutorResponseDto result = tutorService.updateTutorById(tutorId, dtoUpdate);
+
+            // Assert
+            verify(tutorRepository).findById(tutorId);
+            verify(tutorRepository).existsByEmail(dtoUpdate.email());
+            verify(tutorRepository).save(tutor);
+
+            assertEquals(dtoUpdate.email(), tutor.getEmail());
+            assertEquals(dtoUpdate.phone(), tutor.getPhone());
+
+            assertNotNull(result);
+            assertEquals(dtoUpdate.email(), result.email());
+            assertEquals(dtoUpdate.phone(), result.phone());
         }
     }
 }
