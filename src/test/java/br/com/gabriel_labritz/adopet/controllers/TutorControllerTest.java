84faaf1c869762f0989e0dac0870dcadbfeb0740
Gabriel Labritz <gabriellabritz@gmail.com>
2026-03-30 12:17:11 -0300

    test: add unit tests for TutorController getTutor method

--- src/test/java/br/com/gabriel_labritz/adopet/controllers/TutorControllerTest.java
+++ src/test/java/br/com/gabriel_labritz/adopet/controllers/TutorControllerTest.java
@@ -2,8 +2,10 @@ package br.com.gabriel_labritz.adopet.controllers;
 
 import br.com.gabriel_labritz.adopet.dto.tutor.TutorRequestDto;
 import br.com.gabriel_labritz.adopet.dto.tutor.TutorResponseDto;
+import br.com.gabriel_labritz.adopet.dto.tutor.TutorUpdateDto;
 import br.com.gabriel_labritz.adopet.enums.errors.ErrosMessages;
 import br.com.gabriel_labritz.adopet.exceptions.DuplicationExistsException;
+import br.com.gabriel_labritz.adopet.exceptions.NotFoundException;
 import br.com.gabriel_labritz.adopet.services.TutorService;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
@@ -20,8 +22,7 @@ import org.springframework.test.web.servlet.MockMvc;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -37,6 +38,9 @@ class TutorControllerTest {
 
     @Autowired
     JacksonTester<TutorRequestDto> tutorRequestDtoTester;
+
+    @Autowired
+    JacksonTester<TutorUpdateDto> tutorUpdateDtoTester;
 
     @Nested
     class registerTutor {
@@ -94,6 +98,54 @@ class TutorControllerTest {
 
             // Assert
             verify(tutorService).register(any(TutorRequestDto.class));
+        }
+    }
+
+    @Nested
+    class getTutor {
+        @Test
+        @DisplayName("Deve retornar status 200 quando tutor for encontrado.")
+        void shouldReturn200StatusForTutorFound() throws Exception {
+            // Arrange
+            Long tutorId = 2L;
+            TutorResponseDto response = new TutorResponseDto("Jonh", "jonh@gmail.com", "11987654321");
+
+            when(tutorService.getTutorById(tutorId)).thenReturn(response);
+
+            // Act + Assert
+            mvc.perform(get("/tutor/{id}", tutorId))
+                    .andExpect(status().isOk())
+                    .andExpect(jsonPath("$.name").value(response.name()))
+                    .andExpect(jsonPath("$.email").value(response.email()))
+                    .andExpect(jsonPath("$.phone").value(response.phone()));
+        }
+
+        @Test
+        @DisplayName("Deve retornar status 400 quando o id é inválido.")
+        void shouldReturn400WhenInvalidId() throws Exception {
+            // Arrange
+            Long tutorId = -2L;
+
+            // Act + Assert
+            mvc.perform(get("/tutor/{id}", tutorId))
+                    .andExpect(status().isBadRequest());
+        }
+
+        @Test
+        @DisplayName("Deve retornar status 404 quando o tutor não for encontrado.")
+        void shouldReturn404StatusWhenTutorNotFound() throws Exception {
+            // Arrange
+            Long tutorId = 2L;
+
+            when(tutorService.getTutorById(tutorId)).thenThrow(new NotFoundException(ErrosMessages.TUTOR_NOTFOUND.getErrorMessage()));
+
+            // Act + Assert
+            mvc.perform(get("/tutor/{id}", tutorId))
+                    .andExpect(status().isNotFound())
+                    .andExpect(jsonPath("$.detail").value("O Tutor não foi encontrado."))
+                    .andExpect(jsonPath("$.instance").value("/tutor/2"))
+                    .andExpect(jsonPath("$.status").value("404"))
+                    .andExpect(jsonPath("$.title").value("Not Found"));
         }
     }
 }
