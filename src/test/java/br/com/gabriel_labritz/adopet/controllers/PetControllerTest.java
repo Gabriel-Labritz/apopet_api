3f60349b32572a059c63578898cd415574fa5ac5
Gabriel Labritz <gabriellabritz@gmail.com>
2026-03-31 12:48:03 -0300

    test: add unit tests for PetController updatePet method

--- src/test/java/br/com/gabriel_labritz/adopet/controllers/PetControllerTest.java
+++ src/test/java/br/com/gabriel_labritz/adopet/controllers/PetControllerTest.java
@@ -2,9 +2,11 @@ package br.com.gabriel_labritz.adopet.controllers;
 
 import br.com.gabriel_labritz.adopet.dto.pets.PetRequestDto;
 import br.com.gabriel_labritz.adopet.dto.pets.PetResponseDto;
+import br.com.gabriel_labritz.adopet.dto.pets.UpdatePetDto;
 import br.com.gabriel_labritz.adopet.dto.shelter.ShelterResponseDto;
 import br.com.gabriel_labritz.adopet.enums.TypePet;
 import br.com.gabriel_labritz.adopet.enums.errors.ErrosMessages;
+import br.com.gabriel_labritz.adopet.exceptions.AdoptionBusinessException;
 import br.com.gabriel_labritz.adopet.exceptions.NotFoundException;
 import br.com.gabriel_labritz.adopet.services.PetService;
 import org.junit.jupiter.api.DisplayName;
@@ -19,6 +21,7 @@ import org.springframework.http.MediaType;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.io.IOException;
 import java.util.List;
 
 import static org.mockito.ArgumentMatchers.any;
@@ -40,6 +43,9 @@ class PetControllerTest {
 
     @Autowired
     private JacksonTester<PetRequestDto> petRequestDtoTester;
+
+    @Autowired
+    private JacksonTester<UpdatePetDto> petUpdateDtoTester;
 
     @Nested
     class registerPet {
@@ -108,7 +114,7 @@ class PetControllerTest {
                     .andExpect(jsonPath("$.detail").value("O abrigo não foi encontrado."))
                     .andExpect(jsonPath("$.instance").value("/pets"))
                     .andExpect(jsonPath("$.status").value("404"))
-                    .andExpect(jsonPath("$.title").value("Not Found"));;
+                    .andExpect(jsonPath("$.title").value("Not Found"));
 
         }
     }
@@ -230,4 +236,108 @@ class PetControllerTest {
         }
     }
 
+    @Nested
+    class updatePet {
+        @Test
+        @DisplayName("Deve retornar status 200 quando o pet for atualizado.")
+        void shouldReturn200StatusWhenPetIsUpdated() throws Exception {
+            // Arrange
+            Long petId = 2L;
+            ShelterResponseDto shelter = new ShelterResponseDto(2L, "shelter2@gmail.com", "1188888888");
+            UpdatePetDto request = new UpdatePetDto(13, 5.2, 2L);
+            PetResponseDto response = new PetResponseDto(
+                    2L,
+                    "Sophia",
+                    TypePet.GATO,
+                    "Siâmes", request.age(),
+                    request.weight(),
+                    "Marrom", shelter);
+
+            when(petService.updatePetById(petId, request)).thenReturn(response);
+
+            // Act + Assert
+            mvc.perform(patch("/pets/{id}", petId)
+                            .contentType(MediaType.APPLICATION_JSON)
+                            .content(petUpdateDtoTester.write(request).getJson()))
+                    .andExpect(status().isOk())
+                    .andExpect(jsonPath("$.id").value(response.id()))
+                    .andExpect(jsonPath("$.name").value(response.name()))
+                    .andExpect(jsonPath("$.type").value(response.type().name()))
+                    .andExpect(jsonPath("$.breed").value(response.breed()))
+                    .andExpect(jsonPath("$.age").value(response.age()))
+                    .andExpect(jsonPath("$.weight").value(response.weight()))
+                    .andExpect(jsonPath("$.color").value(response.color()))
+                    .andExpect(jsonPath("$.shelter.id").value(response.shelter().id()))
+                    .andExpect(jsonPath("$.shelter.email").value(response.shelter().email()))
+                    .andExpect(jsonPath("$.shelter.phone").value(response.shelter().phone()));
+
+            // Assert
+            verify(petService).updatePetById(petId, request);
+        }
+
+        @Test
+        @DisplayName("Deve retornar status 400 quando o id pet é inválido.")
+        void shouldReturn400StatusWhenPetIdIsInvalid() throws Exception {
+            // Arrange
+            Long petId = -2L;
+
+            // Act + Assert
+            mvc.perform(patch("/pets/{id}", petId))
+                    .andExpect(status().isBadRequest());
+        }
+
+        @Test
+        @DisplayName("Deve retornar status 400 quando os dados de atualização do pet são inválidos.")
+        void shouldReturn400StatusWhenUpdatePetDataIsInvalid() throws Exception {
+            // Arrange
+            Long petId = 2L;
+            UpdatePetDto request = new UpdatePetDto(-13, -5.2, -2L);
+
+            // Act + Assert
+            mvc.perform(patch("/pets/{id}", petId)
+                            .contentType(MediaType.APPLICATION_JSON)
+                            .content(petUpdateDtoTester.write(request).getJson()))
+                    .andExpect(status().isBadRequest());
+        }
+
+        @Test
+        @DisplayName("Deve retornar status 404 quando o pet não for encontrado.")
+        void shouldReturn404StatusWhenPetNotFound() throws Exception {
+            // Arrange
+            Long petId = 2L;
+            UpdatePetDto request = new UpdatePetDto(13, 5.2, 2L);
+
+            when(petService.updatePetById(petId, request)).thenThrow(new NotFoundException(ErrosMessages.PET_NOTFOUND.getErrorMessage()));
+
+            // Act + Assert
+            mvc.perform(patch("/pets/{id}", petId)
+                            .contentType(MediaType.APPLICATION_JSON)
+                            .content(petUpdateDtoTester.write(request).getJson()))
+                    .andExpect(status().isNotFound())
+                    .andExpect(jsonPath("$.detail").value("O pet não foi encontrado."))
+                    .andExpect(jsonPath("$.instance").value("/pets/2"))
+                    .andExpect(jsonPath("$.status").value("404"))
+                    .andExpect(jsonPath("$.title").value("Not Found"));
+        }
+
+        @Test
+        @DisplayName("Deve retornar status 409 quando o pet que será atualizado já foi adotado.")
+        void shouldReturn409StatusWhenPetAlreadyAdopted() throws Exception {
+            // Arrange
+            Long petId = 2L;
+            UpdatePetDto request = new UpdatePetDto(13, 5.2, 2L);
+
+            when(petService.updatePetById(petId, request)).thenThrow(new AdoptionBusinessException(ErrosMessages.UPDATE_PET_ADOPTED.getErrorMessage()));
+
+            // Act + Assert
+            mvc.perform(patch("/pets/{id}", petId)
+                            .contentType(MediaType.APPLICATION_JSON)
+                            .content(petUpdateDtoTester.write(request).getJson()))
+                    .andExpect(status().isConflict())
+                    .andExpect(jsonPath("$.detail").value("Não é possível atualizar um pet já adotado."))
+                    .andExpect(jsonPath("$.instance").value("/pets/2"))
+                    .andExpect(jsonPath("$.status").value("409"))
+                    .andExpect(jsonPath("$.title").value("Conflict"));
+        }
+    }
 }
